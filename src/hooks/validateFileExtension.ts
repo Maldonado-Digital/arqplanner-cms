5c22e6cc650e7275674a1e354babb4eedc6b8eb4
bernardo.amim <bernardo.amim@mercatus.com>
2024-05-14 04:19:34 -0300

    feat: add support for other mime types

--- src/hooks/validateFileExtension.ts
+++ src/hooks/validateFileExtension.ts
@@ -1,6 +1,8 @@
 import { APIError } from 'payload/errors'
 import type { CollectionBeforeValidateHook } from 'payload/types'
 
+const VALID_EXTENSIONS = ['dwg', 'doc', 'docx', 'xls', 'xlsx', 'pptx']
+
 export const validateFileExtension: CollectionBeforeValidateHook = async ({
   data, // incoming data to update or create with
   req, // full express request
@@ -12,7 +14,7 @@ export const validateFileExtension: CollectionBeforeValidateHook = async ({
 
   const ext = (data.filename as string).split('.').pop()
 
-  if (ext !== 'dwg')
+  if (!VALID_EXTENSIONS.includes(ext))
     throw new APIError('Formato de arquivo não suportado.', 400, undefined, true)
 
   return data // Return data to either create or update a document with
