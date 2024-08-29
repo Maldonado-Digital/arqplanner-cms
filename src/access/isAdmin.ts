78c28d2fa006ebcb5c748099e6475d78e777851d
bernardo.amim <bernardo.amim@mercatus.com>
2024-08-29 04:32:59 -0300

    fix: corrects customer works access control

--- src/access/isAdmin.ts
+++ src/access/isAdmin.ts
@@ -21,6 +21,18 @@ export const isSuperAdminOrAdminOrEditorFieldLevel: FieldAccess<
   unknown,
   User
 > = ({ req: { user } }) => {
-  // Return true or false based on if the user has an admin role
-  return Boolean(user?.role === 'admin' || user?.role === 'editor')
+  // User needs to be logged in
+  if (!user) return false
+
+  // Scenario #0 - Allow all users with a role of 'super_admin' to create Customers
+  if (user.role === 'super_admin') return true
+
+  // Scenario #1 - Allow all users with a role of 'admin' to create Customers
+  if (user.role === 'admin') return true
+
+  // Scenario #2 - Allow users with a role of 'editor' to create Customers inside their own organization
+  if (user.role === 'editor' && user.organization) return true
+
+  // Reject everyone else
+  return false
 }
